1cb682a9cb6a31949601442f38b67c736e0fe366
Fog3211 <fog3211@163.com>
2021-08-11 00:04:29 +0800

    feat: add MyConsole constructor

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,66 @@
-declare global {
-  interface Window {
-    _console: typeof window.console
-  }
-}
+export type Method =
+  | 'debug'
+  | 'info'
+  | 'log'
+  | 'warn'
+  | 'error'
 
 export type Options = {
   /** 匹配规则 */
   include?: (string | RegExp)[]
   /** 排除规则 */
   exclude?: (string | RegExp)[]
-  /** 是否识别颜色 */
-  colorful?: boolean
   /**是否对整个console匹配 */
   global?: boolean
-  /** 是否展示数据类型 */
-  showType?: boolean
+  /** 过滤等级 */
+  methods?: Method[]
 }
 
-/** 重写console方法 */
-const rewriteConsole = (options: Options) => {
-  console.log('s')
-  window._console = window.console
+const DefaultOptions: Options = {
+  global: true,
+  methods: []
 }
+class MyConsole {
+  private _console: Console
+  private _options: Options
 
-/** 重置console */
-const resetConsole = () => {
-  if (window._console) {
-    window.console = window._console
-  } else {
-    location.reload()
+  constructor(options?: Options) {
+    this._console = window.console
+    this._options = options || DefaultOptions
+  }
+
+  // 计算过滤后的参数
+  private getArguments(args: IArguments) {
+    const { include = [], exclude = [], global = true } = this._options
+    if (include.length === 0 && exclude.length === 0) return args
+    const [arg0, ...restArg] = args
+    if (global === false) { // 只匹配第一个参数
+      if (typeof arg0 === 'string') {
+
+      } else {
+        return args
+      }
+    } else {
+
+    }
+    return args
+  }
+  // 过滤函数
+  public filter(options: Options) {
+    this._options = Object.assign({}, this._options, options)
+    const { methods = [] } = this._options
+    const availableMethods: Method[] = methods.length === 0 ? ['debug', 'info', 'log', 'warn', 'error'] : methods
+
+    availableMethods.forEach(method => {
+      window.console[method].prototype = () => {
+        this._console[method].apply(this._console, this.getArguments(arguments) as any);
+      };
+    })
+  }
+  /** 重置console */
+  public reset() {
+    window.console = this._console
   }
 }
 
-export {
-  rewriteConsole,
-  resetConsole
-}
+export default MyConsole
